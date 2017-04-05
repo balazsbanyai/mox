e4be22ad94c8364d85309993724083d5a36907a5
Balazs Banyai <balazs.banyai@logmein.com>
2017-04-05 23:55:34 +0200

    draft fix for looking at the classpath before annotating

--- src/main/java/com/banyaibalazs/createmoxplugin/MissingMockAnnotator.java
+++ src/main/java/com/banyaibalazs/createmoxplugin/MissingMockAnnotator.java
@@ -1,15 +1,45 @@
 package com.banyaibalazs.createmoxplugin;
 
-import com.intellij.lang.annotation.*;
-import com.intellij.psi.*;
+import com.intellij.codeInsight.completion.AllClassesGetter;
+import com.intellij.codeInsight.completion.PrefixMatcher;
+import com.intellij.lang.annotation.AnnotationHolder;
+import com.intellij.lang.annotation.Annotator;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.util.Condition;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiConstructorCall;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiParameterList;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.searches.AllClassesSearch;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.util.Processor;
 import org.jetbrains.annotations.NotNull;
 
 public class MissingMockAnnotator implements Annotator {
-    @Override
-    public void annotate(@NotNull final PsiElement element, @NotNull AnnotationHolder holder) {
 
-        if (element instanceof PsiConstructorCall) {
+
+    class ProcessorImpl implements Processor<PsiClass> {
+
+        private final PsiElement element;
+        private final AnnotationHolder holder;
+
+        public ProcessorImpl(PsiElement element, AnnotationHolder holder) {
+            this.element = element;
+            this.holder = holder;
+        }
+
+        public boolean process(PsiClass psiClass) {
+            if (psiClass.getQualifiedName().startsWith("org.mockito.Mock")) {
+                doActualAnnotation();
+                return false;
+            }
+            return true;
+        }
+
+        private void doActualAnnotation() {
             PsiConstructorCall psiConstructorCall = ((PsiConstructorCall) element);
             int countOfActualArgs = psiConstructorCall.getArgumentList().getExpressions().length;
             PsiParameterList parameterList = psiConstructorCall.resolveConstructor().getParameterList();
@@ -19,6 +49,22 @@ public class MissingMockAnnotator implements Annotator {
                         .createErrorAnnotation(element.getTextRange(), "Create mocks for this element")
                         .registerFix(new CreateMocksQuickFix(clazz, psiConstructorCall));
             }
+        }
+    }
+    @Override
+    public void annotate(@NotNull final PsiElement element, @NotNull AnnotationHolder holder) {
+
+        if (element instanceof PsiConstructorCall) {
+            Project project = element.getProject();
+            Module module = ProjectRootManager.getInstance(project).getFileIndex().getModuleForFile(element.getContainingFile().getVirtualFile());
+
+            Processor p = new ProcessorImpl(element, holder);
+            AllClassesGetter.processJavaClasses(
+                    PrefixMatcher.ALWAYS_TRUE,
+                    project,
+                    GlobalSearchScope.moduleWithDependenciesAndLibrariesScope(module),
+                    p
+            );
         }
     }
 }
